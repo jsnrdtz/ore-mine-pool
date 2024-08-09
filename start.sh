025158c86ecc4ab444f07a833e31f23694363032
jsnrdtz <108187128+jsnrdtz@users.noreply.github.com>
2024-08-09 08:09:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 启动命令和参数
-COMMAND="nohup ./ore-mine-pool-linux worker --server-url http://47.254.156.237:7891/ --threads 96  --worker-wallet-address BqM1gY674y4NMcCjvjfN3jPQ2i7pLuFyTu42RircdJ6N  >> worker.log 2>&1 &"
+COMMAND="nohup ./ore-mine-pool-linux worker --server-url http://47.254.156.237:7891/ --threads 123  --worker-wallet-address BqM1gY674y4NMcCjvjfN3jPQ2i7pLuFyTu42RircdJ6N  >> worker.log 2>&1 &"
 
 # 启动进程的函数
 start_process() {
